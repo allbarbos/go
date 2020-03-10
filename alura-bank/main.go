68e19c342e6c2e9a30f0419b5b58fe1dc8f4252b
Allan Barbosa <allan.barbosa@resultadosdigitais.com.br>
2020-03-10 10:36:51 -0300

    interface

--- alura-bank/main.go
+++ alura-bank/main.go
@@ -7,14 +7,24 @@ import (
 	"github.com/go/alura-bank/contas"
 )
 
+type verificarConta interface {
+	Sacar(valor float64)
+}
+
+// PagarBoleto realiza o pagamento
+func PagarBoleto(conta verificarConta, valor float64) {
+	conta.Sacar(valor)
+}
+
 func main() {
 	clienteAllan := clientes.Titular{Nome: "Allan Barbosa", Cpf: "12345356545", Profissao: "Dev"}
 	cc := contas.ContaCorrente{Titular: clienteAllan, Agencia: 0001, Conta: 123456}
 	cc.Depositar(1000)
+	PagarBoleto(&cc, 500)
 	fmt.Println(cc.Saldo())
 
 	pp := contas.ContaPoupanca{}
 	pp.Depositar(35)
-	pp.Sacar(5)
+	PagarBoleto(&pp, 30)
 	fmt.Println(pp.Saldo())
 }
